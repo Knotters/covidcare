a955da3e8ddbe81c6ddaf385d2e4e67188fc5e35
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-11 20:49:14 +0530

    Create need imgsrc field

--- needs/models.py
+++ needs/models.py
@@ -6,7 +6,8 @@ class NeedType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(max_length=50)
     about = models.CharField(max_length=100, default='Browse sources including volunteer verified ones.')
-    icon = models.CharField(max_length=100,default='info')
+    imgsrc = models.CharField(max_length=200, blank=True, null=True)
+    icon = models.CharField(max_length=50, blank=True, null=True)
     def __str__(self):
         return self.type
 
