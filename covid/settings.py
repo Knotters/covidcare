c33fea36172c5b282420649922cfbc8dfc16db80
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-12 20:53:14 +0000

    Fix typo

--- covid/settings.py
+++ covid/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-if not env.PRODUCTION:
+if not env.ISPRODUCTION:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
