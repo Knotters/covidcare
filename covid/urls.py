08af59ffad6d52f2444e538ee5e4db95aea2bd4a
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-16 15:34:17 +0000

    minor change in url

--- covid/urls.py
+++ covid/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 urlpatterns = [
     path(env.ADMINPATH, admin.site.urls),
+    path('clips', views.mediaclips),
     path('', views.index),
     path('needs/', include('needs.urls')),
-    path('mediaclips/', views.mediaclips),
     # path('volunteer', views.volunteer)
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
